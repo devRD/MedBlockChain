4a7bd2185ca83325aa9635eb252230448ef3e4b9
Lasse Norfeldt <lasse@norfeldt.dk>
2019-05-13 21:06:14 +0200

    Context: product data & dose testing

--- Context.test.js
+++ Context.test.js
@@ -2,21 +2,37 @@ import React from 'react'
 import { create } from 'react-test-renderer'
 import { ContextProvider } from './Context'
 
-describe('ContextProvider', () => {
-  test('it updates dose correctly', () => {
+describe('Testing ContextProvider', () => {
+  // TODO: verify that blockchain instance is in state
+
+  test('Product data: initializing correctly', () => {
+    const component = create(<ContextProvider />)
+    const instance = component.getInstance()
+    const {
+      productionUnit,
+      compound,
+      dose,
+      hashSalt,
+      productionTime,
+    } = instance.state.productData
+
+    expect(productionUnit).toBe('CPHarma')
+    expect(compound).toBe('rosuvastatin')
+    expect(dose).toBe('5 mg')
+    expect(hashSalt).toMatch(/^[A-F0-9]{5}\s[A-F0-9]{5}$/)
+    expect(productionTime).toMatch(/^20\d\d-[0-2]\d-[0-3]\d\s[0-2]\d:[0-6]\d$/)
+  })
+
+  test('Dose: Getting and setting', () => {
     const component = create(<ContextProvider />)
     const instance = component.getInstance()
 
+    // Verify that getting the dose returns a correct number (and not a string)
     expect(instance.getDose()).toBe(5)
-    expect(instance.state.productData.dose).toBe('5 mg')
 
+    // Verify that changing the dose updates and can be retrieved correctly
     instance.setDose(2)
-
-    expect(instance.getDose()).toBe(2)
     expect(instance.state.productData.dose).toBe('2 mg')
-  })
-
-  test('it updates something else correctly', () => {
-    // ...
+    expect(instance.getDose()).toBe(2)
   })
 })
