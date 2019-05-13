546888d379bf456aabb09d5de3a4d595f2620584
Lasse Norfeldt <lasse@norfeldt.dk>
2019-05-13 12:51:09 +0200

    Jest WORKING

--- Context.test.js
+++ Context.test.js
@@ -1,26 +1,22 @@
-// import { ContextProvider } from './Context'
+import React from 'react'
+import { create } from 'react-test-renderer'
+import { ContextProvider } from './Context'
 
-// describe('ContextProvider', () => {
-//   test('it updates dose correctly', () => {
-//     const component = create(<ContextProvider />)
-//     const instance = component.getInstance()
+describe('ContextProvider', () => {
+  test('it updates dose correctly', () => {
+    const component = create(<ContextProvider />)
+    const instance = component.getInstance()
 
-//     expect(instance.getDose()).toBe(5)
-//     expect(instance.state.productData.dose).toBe('5 mg')
+    expect(instance.getDose()).toBe(5)
+    expect(instance.state.productData.dose).toBe('5 mg')
 
-//     instance.setDose(2)
+    instance.setDose(2)
 
-//     expect(instance.getDose()).toBe(2)
-//     expect(instance.state.productData.dose).toBe('2 mg')
-//   })
+    expect(instance.getDose()).toBe(2)
+    expect(instance.state.productData.dose).toBe('2 mg')
+  })
 
-//   test('it updates something else correctly', () => {
-//     // ...
-//   })
-// })
-
-describe('truth', () => {
-  it('is true', () => {
-    expect(true).toEqual(true)
+  test('it updates something else correctly', () => {
+    // ...
   })
 })
