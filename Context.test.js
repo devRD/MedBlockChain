e59131179289eae6b8505e597a4097b60ef0202e
Lasse Norfeldt <lasse@norfeldt.dk>
2019-05-14 10:26:39 +0200

    private functions testing

--- Context.test.js
+++ Context.test.js
@@ -5,7 +5,7 @@ import { ContextProvider } from './Context'
 describe('Testing ContextProvider', () => {
   // TODO: verify that blockchain instance is in state
 
-  test('Product data: initializing correctly', () => {
+  test('state: productData - initializing correctly', () => {
     const component = create(<ContextProvider />)
     const instance = component.getInstance()
     const {
@@ -23,6 +23,44 @@ describe('Testing ContextProvider', () => {
     expect(productionTime).toMatch(/^20\d\d-[0-2]\d-[0-3]\d\s[0-2]\d:[0-6]\d$/)
   })
 
+  // Private functions
+  test('makeHashSalt (private function)', () => {
+    const component = create(<ContextProvider />)
+    const instance = component.getInstance()
+    const hashSalt = instance.makeHashSalt()
+    expect(hashSalt).toMatch(/^[A-F0-9]{5}\s[A-F0-9]{5}$/)
+  })
+
+  test('getDefaultproductData (private function)', () => {
+    const component = create(<ContextProvider />)
+    const instance = component.getInstance()
+
+    // Calling it with all arguments
+    const [productionUnitTest, productionTimeTest, hashSaltTest] = [
+      'Company Name',
+      '2020-12-24 24:00',
+      '009FG ABCDE',
+    ]
+    const {
+      productionUnit,
+      compound,
+      dose,
+      hashSalt,
+      productionTime,
+    } = instance.getDefaultproductData(
+      productionUnitTest,
+      productionTimeTest,
+      hashSaltTest
+    )
+
+    expect(productionUnit).toBe(productionUnitTest)
+    expect(compound).toBe('rosuvastatin')
+    expect(dose).toBe('5 mg')
+    expect(hashSalt).toBe(hashSaltTest)
+    expect(productionTime).toMatch(productionTimeTest)
+  })
+
+  // Public funtions
   test('Dose: Getting and setting', () => {
     const component = create(<ContextProvider />)
     const instance = component.getInstance()
