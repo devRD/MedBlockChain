6593315d6e3b6bada557c592704b499fc03f892f
Lasse Norfeldt <lasse@norfeldt.dk>
2019-05-15 11:05:49 +0200

    refactor

--- Context.test.js
+++ Context.test.js
@@ -2,12 +2,31 @@ import React from 'react'
 import { create } from 'react-test-renderer'
 import { ContextProvider } from './Context'
 
+// Avoid repeating code by always initiating an instance
+let instance = null
+beforeEach(() => {
+  const component = create(<ContextProvider />)
+  instance = component.getInstance()
+})
+
+const [
+  default_productionUnit,
+  default_compound,
+  default_dose,
+  default_hashSalt,
+  default_productionTime,
+] = [
+  'CPHarma',
+  'rosuvastatin',
+  '5 mg',
+  /^[A-F0-9]{5}\s[A-F0-9]{5}$/,
+  /^20\d\d-[0-2]\d-[0-3]\d\s[0-2]\d:[0-6]\d$/,
+]
+
 describe('Testing ContextProvider', () => {
   // TODO: verify that blockchain instance is in state
 
   test('state: productData - initializing correctly', () => {
-    const component = create(<ContextProvider />)
-    const instance = component.getInstance()
     const {
       productionUnit,
       compound,
@@ -16,25 +35,20 @@ describe('Testing ContextProvider', () => {
       productionTime,
     } = instance.state.productData
 
-    expect(productionUnit).toBe('CPHarma')
-    expect(compound).toBe('rosuvastatin')
-    expect(dose).toBe('5 mg')
-    expect(hashSalt).toMatch(/^[A-F0-9]{5}\s[A-F0-9]{5}$/)
-    expect(productionTime).toMatch(/^20\d\d-[0-2]\d-[0-3]\d\s[0-2]\d:[0-6]\d$/)
+    expect(productionUnit).toBe(default_productionUnit)
+    expect(compound).toBe(default_compound)
+    expect(dose).toBe(default_dose)
+    expect(hashSalt).toMatch(default_hashSalt)
+    expect(productionTime).toMatch(default_productionTime)
   })
 
   // Private functions
   test('makeHashSalt (private function)', () => {
-    const component = create(<ContextProvider />)
-    const instance = component.getInstance()
     const hashSalt = instance.makeHashSalt()
-    expect(hashSalt).toMatch(/^[A-F0-9]{5}\s[A-F0-9]{5}$/)
+    expect(hashSalt).toMatch(default_hashSalt)
   })
 
   test('getDefaultproductData (private function)', () => {
-    const component = create(<ContextProvider />)
-    const instance = component.getInstance()
-
     // Calling it with all arguments
     const [productionUnitTest, productionTimeTest, hashSaltTest] = [
       'Company Name',
@@ -54,17 +68,14 @@ describe('Testing ContextProvider', () => {
     )
 
     expect(productionUnit).toBe(productionUnitTest)
-    expect(compound).toBe('rosuvastatin')
-    expect(dose).toBe('5 mg')
+    expect(compound).toBe(default_compound)
+    expect(dose).toBe(default_dose)
     expect(hashSalt).toBe(hashSaltTest)
     expect(productionTime).toMatch(productionTimeTest)
   })
 
   // Public funtions
   test('Dose: Getting and setting', () => {
-    const component = create(<ContextProvider />)
-    const instance = component.getInstance()
-
     // Verify that getting the dose returns a correct number (and not a string)
     expect(instance.getDose()).toBe(5)
 
@@ -75,9 +86,6 @@ describe('Testing ContextProvider', () => {
   })
 
   test('Dose range: Getting', () => {
-    const component = create(<ContextProvider />)
-    const instance = component.getInstance()
-
     expect(instance.getDoseRange()).toEqual({ minDose: 0, maxDose: 40 })
   })
 })
